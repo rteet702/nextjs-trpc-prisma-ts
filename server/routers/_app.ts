8e9db6bf50975c76d3ae44db916237d94af7eec3
Robert Teets <teetsrobert@gmail.com>
2022-10-26 16:07:03 -0600

    feat: successful trpc + prisma mutation

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,13 +1,54 @@
 import { z } from "zod";
+import { hash } from "bcrypt";
 import { router, publicProcedure } from "../trpc";
 
+interface User {
+    id: String;
+    firstName: String;
+    lastName: String;
+    email: String;
+    password: String;
+}
+
+interface Message {
+    id: String;
+    author: User;
+    content: String;
+}
+
 export const appRouter = router({
-    getAll: publicProcedure.query(async ({ ctx }) => {
+    getAllUsers: publicProcedure.query(async ({ ctx }) => {
         const users = await ctx.prisma.user.findMany();
         return {
             users: users,
         };
     }),
+    createUser: publicProcedure
+        .input(
+            z.object({
+                firstName: z.string(),
+                lastName: z.string(),
+                email: z.string(),
+                password: z.string(),
+            })
+        )
+        .mutation(async ({ input, ctx }) => {
+            // Something here...
+            const { firstName, lastName, email, password } = input;
+            const pwHash = await hash(password, 10);
+            const user = await ctx.prisma.user.create({
+                data: {
+                    firstName,
+                    lastName,
+                    email,
+                    password: pwHash,
+                },
+            });
+            return {
+                // Something... eventually...
+                user,
+            };
+        }),
 });
 
 export type AppRouter = typeof appRouter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
 import { trpc } from "../../utils/trpc";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    const allUsers = trpc.getAll.useQuery();
+    const allUsers = trpc.getAllUsers.useQuery();
+    const [formState, setFormState] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+    });
 
-    console.log(allUsers?.data);
+    const userMutate = trpc.createUser.useMutation({});
+
+    const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const { firstName, lastName, email, password } = formState;
+
+        userMutate.mutate({
+            firstName,
+            lastName,
+            email,
+            password,
+        });
+    };
+
+    const changeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+        const target = e.target as HTMLInputElement;
+        setFormState({ ...formState, [target.name]: target.value });
+    };
+
+    useEffect(() => {
+        console.log(formState);
+    }, [formState]);
 
     if (!allUsers.data) {
         return <div>Loading...</div>;
@@ -11,7 +39,47 @@ export default function Home() {
 
     return (
         <div>
-            <p></p>
+            <form
+                onSubmit={submitHandler}
+                className="flex flex-col w-[500px] gap-4 mx-auto bg-neutral-800 p-4 shadow-lg"
+            >
+                <input
+                    type="text"
+                    className="bg-neutral-700"
+                    name="firstName"
+                    onChange={changeHandler}
+                    value={formState.firstName}
+                />
+                <input
+                    type="text"
+                    className="bg-neutral-700"
+                    name="lastName"
+                    onChange={changeHandler}
+                    value={formState.lastName}
+                />
+                <input
+                    type="text"
+                    className="bg-neutral-700"
+                    name="email"
+                    onChange={changeHandler}
+                    value={formState.email}
+                />
+                <input
+                    type="text"
+                    className="bg-neutral-700"
+                    name="password"
+                    onChange={changeHandler}
+                    value={formState.password}
+                />
+                <button type="submit" className="bg-purple-900">
+                    Submit
+                </button>
+            </form>
+            <div>
+                {allUsers.data.users.map((user, index: number) => {
+                    return <div key={index}>{user.firstName}</div>;
+                })}
+            </div>
         </div>
     );
 }
